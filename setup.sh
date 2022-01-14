21a90fefbe656a03b085fa4bac76ba4bdeb2d537
Brian Jackson <bjack205@gmail.com>
2022-01-14 12:47:48 -0500

    Add git config

--- setup.sh
+++ setup.sh
@@ -26,6 +26,9 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githu
 sudo apt update
 sudo apt install gh
 
+git config --global user.name "Brian Jackson"
+git config --global user.email "bjack205@gmail.com"
+
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
